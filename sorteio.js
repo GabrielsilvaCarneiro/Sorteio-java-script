04d70f1d966377c7998f78c8fd6580baf6897ee6
Gabriel Carneiro <133365361+GabrielsilvaCarneiro@users.noreply.github.com>
2023-08-04 14:37:46 -0300

    sorteio java

---
+++ sorteio.js
@@ -0,0 +1,19 @@
+//Math
+console.log(Math.random());
+console.log(Math.random()*20);
+console.log(Math.random(7-3+1)+3);
+
+
+//Arredondamento valor inteiro
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+
+
+//Arredondamento para menos 
+console.log(Math.floor(3.1));
+console.log(Math.floor(3.9));
+
+
+//Arredondamento para mais
+console.log(Math.ceil(3.1));
+console.log(Math.ceil(3.9));
